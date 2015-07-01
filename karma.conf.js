e8cd6c8d011e063dd12d84a7bf2e6f1da402592e
dongwoo <dongwoo.kim@nhnent.com>
2015-07-01 10:40:51 +0900

    update karma.conf.js for coverage

--- karma.conf.js
+++ karma.conf.js
@@ -144,6 +144,12 @@ module.exports = function(config) {
         exclude: [
         ],
 
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+          'src/**/*.js': ['coverage']
+        },
+
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
