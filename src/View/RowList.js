6d068ba6159eef933831ad3286f98ec2b0932cc4
dongwoo <dongwoo.kim@nhnent.com>
2015-07-06 10:55:36 +0900

    refine, add comment View.RowList

--- src/View/RowList.js
+++ src/View/RowList.js
@@ -22,7 +22,7 @@ View.RowList = View.Base.extend(/**@lends View.RowList.prototype */{
             timeoutIdForCollection: 0,
             timeoutIdForFocusClipboard: 0,
             sortOptions: _.clone(this.grid.dataModel.sortOptions),
-            rowIds: null,
+            renderedRowKeys: null,
             rowPainter: null
         });
         this._createRowPainter();
@@ -42,32 +42,40 @@ View.RowList = View.Base.extend(/**@lends View.RowList.prototype */{
     },
 
     /**
-     * 기존에 생성되어 있던 TR요소들 중 목록에서 사라진 요소들을 삭제한다.
-     * @param {array}
+     * 기존에 생성되어 있던 TR요소들 중 새로 렌더링할 데이터와 중복되지 않은 목록의 TR요소만 삭제한다.
+     * @param {array} dupRowKeys 중복된 데이터의 rowKey 목록
      */
-    _removeOldRows: function(dupRowIds) {
-        var firstIdx = _.indexOf(this.rowIds, dupRowIds[0]);
-            lastIdx = _.indexOf(this.rowIds, _.last(dupRowIds)),
+    _removeOldRows: function(dupRowKeys) {
+        var firstIdx = _.indexOf(this.renderedRowKeys, dupRowKeys[0]);
+            lastIdx = _.indexOf(this.renderedRowKeys, _.last(dupRowKeys)),
             $rows = this.$el.children('tr');
 
         $rows.slice(0, firstIdx).remove();
         $rows.slice(lastIdx + 1).remove();
     },
 
-    _appendNewRows: function(rowIds, dupRowIds) {
-        var beforeRows = this.collection.slice(0, _.indexOf(rowIds, dupRowIds[0])),
-            afterRows = this.collection.slice(_.indexOf(rowIds, _.last(dupRowIds)) + 1);
+    /**
+     * 기존의 렌더링된 데이터와 중복되지 않은 목록에 대해서만 TR요소를 추가한다.
+     * @param {array} rowKeys 렌더링할 데이터의 rowKey 목록
+     * @param {array} dupRowKeys 중복된 데이터의 rowKey 목록
+     */
+    _appendNewRows: function(rowKeys, dupRowKeys) {
+        var beforeRows = this.collection.slice(0, _.indexOf(rowKeys, dupRowKeys[0])),
+            afterRows = this.collection.slice(_.indexOf(rowKeys, _.last(dupRowKeys)) + 1);
 
         this.$el.prepend(this._getRowsHtml(beforeRows));
         this.$el.append(this._getRowsHtml(afterRows));
     },
 
     /**
-     * el의 innerHTML을 변경한다.
+     * 전체 행목록을 갱신한다.
      */
-    _resetInnerHTML: function(html) {
-        // IE7-9 에서 tbody의 innerHTML를 직접 변경할 수 없음
-        if (ne.util.browser.msie && ne.util.browser.version <= 9) {
+    _resetRows: function() {
+        var html = this._getRowsHtml(this.collection.models)
+
+        // Tbody에 innerHTML을 사용할 수 없는 경우 jquery의 append() 명령을 사용한다.
+        // (Tbody의 대해서는 jquery의 html() 내부적으로 append() 명령을 사용함)
+        if (View.RowList.isInnerHtmlOfTbodyReadOnly) {
             this.$el.empty().append(html);
         } else {
             this.$el[0].innerHTML = html;
@@ -75,7 +83,9 @@ View.RowList = View.Base.extend(/**@lends View.RowList.prototype */{
     },
 
     /**
-     *
+     * 행데이터 목록을 받아, HTML 문자열을 생성해서 반환한다.
+     * @param rows {Model.Row[]} 행데이터 목록
+     * @return {string} 생성된 HTML 문자열
      */
     _getRowsHtml: function(rows) {
         var html = _.reduce(rows, function(memo, row) {
@@ -85,6 +95,10 @@ View.RowList = View.Base.extend(/**@lends View.RowList.prototype */{
         return html;
     },
 
+    /**
+     * timeout을 사용해 일정 시간이 지난 후 포커스를 Clipboard로 옮긴다.
+     * @param {number} delay 지연시킬 시간 (ms)
+     */
     _focusClipboard: function(delay) {
         clearTimeout(this.timeoutIdForFocusClipboard);
         this.timeoutIdForFocusClipboard = setTimeout($.proxy(function() {
@@ -97,32 +111,35 @@ View.RowList = View.Base.extend(/**@lends View.RowList.prototype */{
      * @return {View.RowList} this 객체
      */
     render: function() {
-        var rowIds = this.collection.pluck('rowKey'),
+        var rowKeys = this.collection.pluck('rowKey'),
             sortOptions = _.clone(this.grid.dataModel.sortOptions),
             isReset = false,
-            dupRowIds;
+            dupRowKeys;
 
+        // 정렬방식이 변경된 경우는 무조건 새로 그린다.
         if (!_.isEqual(sortOptions, this.sortOptions)) {
             isReset = true;
         } else {
-            dupRowIds = _.intersection(rowIds, this.rowIds);
-            if (_.isEmpty(rowIds) || _.isEmpty(dupRowIds) || (dupRowIds.length / rowIds.length > 0.8)) {
+            dupRowKeys = _.intersection(rowKeys, this.renderedRowKeys);
+            if (_.isEmpty(rowKeys) || _.isEmpty(dupRowKeys) ||
+                // 중복된 데이터가 80% 이상인 경우에는 remove/append 하는 것보다 innerHTML을 사용하는 게 더 빠름
+                (!View.RowList.isInnerHtmlOfTbodyReadOnly && dupRowKeys.length / rowKeys.length > 0.8)) {
                 isReset = true;
             }
         }
 
         if (isReset) {
-            this._resetInnerHTML(this._getRowsHtml(this.collection.models));
+            this._resetRows();
         } else {
-            this._removeOldRows(dupRowIds);
-            this._appendNewRows(rowIds, dupRowIds);
+            this._removeOldRows(dupRowKeys);
+            this._appendNewRows(rowKeys, dupRowKeys);
         }
 
         this.rowPainter.detachHandlerAll();
         this.destroyChildren();
         this._createRowPainter();
 
-        this.rowIds = rowIds;
+        this.renderedRowKeys = rowKeys;
         this.sortOptions = sortOptions;
 
         this.rowPainter.attachHandlerAll();
@@ -152,4 +169,10 @@ View.RowList = View.Base.extend(/**@lends View.RowList.prototype */{
     //_onMouseDown: function(mouseDownEvent) {
     //    this.grid.selection.onMouseDown(mouseDownEvent);
     //}
+}, {
+    /**
+     * @static
+     * tbody 요소의 innerHTML이 읽기전용인지 여부
+     */
+    isInnerHtmlOfTbodyReadOnly: (ne.util.browser.msie && ne.util.browser.version <= 9)
 });
