0b8a6e697e8ed211599f6db31e35a9ce12317361
dongwoo <dongwoo.kim@nhnent.com>
2016-05-18 18:04:12 +0900

    update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -42,7 +42,6 @@ module.exports = function(config) {
 
             'src/js/**/*.js',
             'test/**/*.spec.js'
-            // 'test/js/addon/net.spec.js'
         ],
 
         // list of files to exclude
@@ -157,6 +156,6 @@ module.exports = function(config) {
         },
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false
+        singleRun: true
     });
 };
