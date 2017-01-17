1f06500d72b818cd49d6f9761ac1bfc5d66f0526
dongwoo <dongwoo.kim@nhnent.com>
2017-01-17 15:20:30 +0900

    chore: update e2e test configs

--- wdio.conf.js
+++ wdio.conf.js
@@ -67,32 +67,32 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 5,
+    maxInstances: 2,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
 
-    // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-    // grid with only 5 firefox instances available you can make sure that not more than
-    // 5 instances get started at a time.
-    // browserName: 'internet explorer',
-    // version: 11
     capabilities: [
+        // {
+        //     browserName: 'internet explorer',
+        //     version: 9
+        // },
+        // {
+        //     browserName: 'internet explorer',
+        //     version: 10
+        // },
         {
-            browserName: 'internet explorer',
-            version: 9
-        },
-        {
-            browserName: 'internet explorer',
-            version: 10
-        },
-        {
+            // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+            // grid with only 5 firefox instances available you can make sure that not more than
+            // 5 instances get started at a time.
+            maxInstances: 1,
             browserName: 'internet explorer',
             version: 11
         },
         {
+            maxInstances: 1,
             browserName: 'chrome'
         }
     ],
