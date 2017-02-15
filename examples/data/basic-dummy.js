c1138440ab3c2ecbf614bd4d40d18be8fd535e65
seonim-ryu <seonim.ryu@nhnent.com>
2017-02-15 19:14:01 +0900

    feat: add new data & basic example

---
+++ examples/data/basic-dummy.js
@@ -0,0 +1,142 @@
+var dummyRowList = [
+    {
+        albumName: 'Beautiful Lies',
+        artist: 'Birdy',
+        type: 'Deluxe',
+        release: '2016.03.26',
+        genre: 'Pop'
+    },
+    {
+        albumName: 'X',
+        artist: 'Ed Sheeran',
+        type: 'Deluxe',
+        release: '2014.06.24',
+        genre: 'Pop'
+    },
+    {
+        albumName: 'Moves Like Jagger',
+        artist: 'Maroon5',
+        type: 'Single',
+        release: '2011.06.27',
+        genre: 'Rock, Pop'
+    },
+    {
+        albumName: 'A Head Full Of Dreams',
+        artist: 'Coldplay',
+        type: 'Deluxe',
+        release: '2015.12.04',
+        genre: 'Rock'
+    },
+    {
+        albumName: '21',
+        artist: 'Adele',
+        type: 'Deluxe',
+        release: '2011.01.21',
+        genre: 'Pop, R&B'
+    },
+    {
+        albumName: 'Warm On A Cold Night',
+        artist: 'HONNE',
+        type: 'EP',
+        release: '2015.05.11',
+        genre: 'R&B, Electronic'
+    },
+    {
+        albumName: 'Take Me To The Alley',
+        artist: 'Gregory Porter',
+        type: 'Deluxe',
+        release: '2016.09.02',
+        genre: 'Jazz'
+    },
+    {
+        albumName: 'Make Out',
+        artist: 'LANY',
+        type: 'EP',
+        release: '2015.12.11',
+        genre: 'Electronic'
+    },
+    {
+        albumName: 'We Sing. We Dance. We Steal Things.',
+        artist: 'Jason Mraz',
+        type: 'Deluxe',
+        release: '2008.05.20',
+        genre: 'Pop'
+    },
+    {
+        albumName: 'Get Lucky',
+        artist: 'Daft Punk',
+        type: 'Single',
+        release: '2013.04.23',
+        genre: 'Funk, Pop'
+    },
+    {
+        albumName: 'Hoppipolla (Planet Earth II Mix)',
+        artist: 'Sigur Rós',
+        type: 'Single',
+        release: '2016.11.22',
+        genre: 'Rock'
+    },
+    {
+        albumName: 'Bush',
+        artist: 'Snoop Dogg',
+        type: 'EP',
+        release: '2015.05.12',
+        genre: 'Hiphop'
+    },
+    {
+        albumName: '4',
+        artist: 'Beyoncé',
+        type: 'EP',
+        release: '2012.01.06',
+        genre: 'Pop'
+    },
+    {
+        albumName: 'I`m Yours',
+        artist: 'Jason Mraz',
+        type: 'Single',
+        release: '2008.03.22',
+        genre: 'Pop'
+    },
+    {
+        albumName: 'Following My Intuition',
+        artist: 'Craig David',
+        type: 'Deluxe',
+        release: '2016.10.01',
+        genre: 'R&B, Electronic'
+    },
+    {
+        albumName: 'Blue Skies',
+        artist: 'Lenka',
+        type: 'Single',
+        release: '2015.03.18',
+        genre: 'Rock, Pop'
+    },
+    {
+        albumName: 'This Is Acting',
+        artist: 'Sia',
+        type: 'EP',
+        release: '2016.10.22',
+        genre: 'Pop'
+    },
+    {
+        albumName: 'Blurryface',
+        artist: 'Twenty One Pilots',
+        type: 'EP',
+        release: '2015.05.19',
+        genre: 'Rock'
+    },
+    {
+        albumName: 'In The Lonely Hour',
+        artist: 'Sam Smith',
+        type: 'Deluxe',
+        release: '2014.09.05',
+        genre: 'Pop, R&B'
+    },
+    {
+        albumName: 'The Magic Whip',
+        artist: 'Blur',
+        type: 'EP',
+        release: '2015.04.27',
+        genre: 'Rock'
+    }
+];
