2b64d5b24d1db8a64e467a0760bb9b38097db0c1
seonim-ryu <seonim.ryu@nhnent.com>
2017-03-07 15:42:29 +0900

    chore: edit karma

--- karma.conf.ne.js
+++ karma.conf.ne.js
@@ -20,9 +20,7 @@ module.exports = function(config) {
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: [
-            'jasmine', 'es5-shim'
-        ],
+        frameworks: ['jasmine', 'es5-shim'],
 
         // list of files / patterns to load in the browser
         files: [
