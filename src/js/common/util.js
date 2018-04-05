419abb92b71aa2ea0b3bc943dd97f100b1c828ba
류선임 <seonim.ryu@nhnent.com>
2018-04-05 16:50:46 +0900

    Feature/update readme (#103)
    * chore: edit protocol http to https in examples
    
    * fix: remove return value of in sendHostNameToGA util method
    
    * chore: update readme
    
    * chore: edit annotation host name to hostname
    
    * chore: update readme (close #104)
    
    * chore: edit spec text to test ci

--- src/js/common/util.js
+++ src/js/common/util.js
@@ -447,12 +447,11 @@ util = {
 
     /**
      * Send information to google analytics
-     * @returns {HTMLElement} created <img /> element to request server
      */
     sendHostNameToGA: function() {
         var hostname = location.hostname;
 
-        return snippet.imagePing('https://www.google-analytics.com/collect', {
+        snippet.imagePing('https://www.google-analytics.com/collect', {
             v: 1,
             t: 'event',
             tid: 'UA-115377265-9',
