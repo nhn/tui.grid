7bb3729db774ce3517eb11968ccb9ed24d170c7d
DongWoo Kim <dongwoo.kim@nhnent.com>
2019-01-30 11:33:04 +0900

    chore: change libraryTarget to commonjs2

--- packages/toast-ui.react-grid/webpack.config.js
+++ packages/toast-ui.react-grid/webpack.config.js
@@ -1,18 +1,20 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const config = {
   entry: './src/index.js',
   output: {
     filename: 'toastui-react-grid.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2'
   },
   externals: {
     'tui-grid': {
       commonjs: 'tui-grid',
-      commonjs2: 'tui-grid',
-      amd: 'tui-grid',
-      root: ['tui', 'Grid']
+      commonjs2: 'tui-grid'
+    },
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react'
     }
   },
   module: {
@@ -31,6 +33,4 @@ const config = {
   }
 };
 
-module.exports = () => {
-  return config;
-};
+module.exports = () => config;
