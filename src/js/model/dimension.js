2f524c362ac526aa3ed15443cfd0737129c0bba8
DongWoo Kim <dongwoo.kim@nhnent.com>
2019-02-21 18:12:52 +0900

    fix: unexpected change of fixedHeight

--- src/js/model/dimension.js
+++ src/js/model/dimension.js
@@ -32,7 +32,6 @@ var Dimension = Model.extend(/** @lends module:model/dimension.prototype */{
         this.domState = options.domState;
 
         this.on('change:fixedHeight', this._resetSyncHeightHandler);
-        this.on('change:bodyHeight', this._onChangeBodyHeight);
 
         if (options.domEventBus) {
             this.listenTo(options.domEventBus, 'windowResize', this._onResizeWindow);
@@ -79,18 +78,18 @@ var Dimension = Model.extend(/** @lends module:model/dimension.prototype */{
     },
 
     /**
-     * Event handler for 'windowResize' event on domEventBus
-     * @private
-     */
+   * Event handler for 'windowResize' event on domEventBus
+   * @private
+   */
     _onResizeWindow: function() {
         this.refreshLayout();
     },
 
     /**
-     * Event handler for 'dragmmove:resizeHgith' event on domEventBus
-     * @param {module:event/gridEvent} ev - GridEvent
-     * @private
-     */
+   * Event handler for 'dragmmove:resizeHgith' event on domEventBus
+   * @param {module:event/gridEvent} ev - GridEvent
+   * @private
+   */
     _onDragMoveForHeight: function(ev) {
         var height = ev.pageY - this.get('offsetTop') - ev.startData.mouseOffsetY;
 
@@ -98,24 +97,9 @@ var Dimension = Model.extend(/** @lends module:model/dimension.prototype */{
     },
 
     /**
-     * Event handler for changing 'bodyHeight' value
-     * @param {object} model - dimension model
-     * @private
-     */
-    _onChangeBodyHeight: function(model) {
-        var changed = model.changed;
-        var changedTotalRowHeight = changed.totalRowHeight;
-        var changedBodyHeight = changed.bodyHeight;
-
-        if (!changedTotalRowHeight && changedBodyHeight) {
-            this.set('fixedHeight', (changedBodyHeight !== 'auto'));
-        }
-    },
-
-    /**
-     * Attach/Detach event handler of change:totalRowHeight event based on the fixedHeight.
-     * @private
-     */
+   * Attach/Detach event handler of change:totalRowHeight event based on the fixedHeight.
+   * @private
+   */
     _resetSyncHeightHandler: function() {
         if (this.get('fixedHeight')) {
             this.off('change:totalRowHeight');
@@ -125,9 +109,9 @@ var Dimension = Model.extend(/** @lends module:model/dimension.prototype */{
     },
 
     /**
-     * Sets the bodyHeight value based on the totalRowHeight value.
-     * @private
-     */
+   * Sets the bodyHeight value based on the totalRowHeight value.
+   * @private
+   */
     _syncBodyHeightWithTotalRowHeight: function() {
         var realBodyHeight = this.get('totalRowHeight') + this.getScrollXHeight();
         var minBodyHeight = this.get('minBodyHeight');
@@ -137,20 +121,20 @@ var Dimension = Model.extend(/** @lends module:model/dimension.prototype */{
     },
 
     /**
-     * Returns whether division border (between meta column and data column) is doubled or not.
-     * Division border should be doubled only if visible fixed data column exists.
-     * @returns {Boolean}
-     */
+   * Returns whether division border (between meta column and data column) is doubled or not.
+   * Division border should be doubled only if visible fixed data column exists.
+   * @returns {Boolean}
+   */
     isDivisionBorderDoubled: function() {
         return this.columnModel.getVisibleFrozenCount() > 0;
     },
 
     /**
-     * 전체 넓이에서 스크롤바, border등의 넓이를 제외하고 실제 셀의 넓이에 사용되는 값만 반환한다.
-     * @param {number} columnLength - 컬럼의 개수
-     * @returns {number} 사용가능한 전체 셀의 넓이
-     * @private
-     */
+   * 전체 넓이에서 스크롤바, border등의 넓이를 제외하고 실제 셀의 넓이에 사용되는 값만 반환한다.
+   * @param {number} columnLength - 컬럼의 개수
+   * @returns {number} 사용가능한 전체 셀의 넓이
+   * @private
+   */
     getAvailableTotalWidth: function(columnLength) {
         var totalWidth = this.get('width');
         var borderCount = columnLength + 1 + (this.isDivisionBorderDoubled() ? 1 : 0);
@@ -160,9 +144,9 @@ var Dimension = Model.extend(/** @lends module:model/dimension.prototype */{
     },
 
     /**
-     * Calc body size of grid except scrollBar
-     * @returns {{height: number, totalWidth: number, rsideWidth: number}} Body size
-     */
+   * Calc body size of grid except scrollBar
+   * @returns {{height: number, totalWidth: number, rsideWidth: number}} Body size
+   */
     getBodySize: function() {
         var lsideWidth = this.get('lsideWidth'),
             rsideWidth = this.get('rsideWidth') - this.getScrollYWidth(),
@@ -176,11 +160,11 @@ var Dimension = Model.extend(/** @lends module:model/dimension.prototype */{
     },
 
     /**
-     * Calc and get overflow values from container position
-     * @param {Number} pageX - Mouse X-position based on page
-     * @param {Number} pageY - Mouse Y-position based on page
-     * @returns {{x: number, y: number}} Mouse-overflow
-     */
+   * Calc and get overflow values from container position
+   * @param {Number} pageX - Mouse X-position based on page
+   * @param {Number} pageY - Mouse Y-position based on page
+   * @returns {{x: number, y: number}} Mouse-overflow
+   */
     getOverflowFromMousePosition: function(pageX, pageY) {
         var containerPos = this.getPositionFromBodyArea(pageX, pageY);
         var bodySize = this.getBodySize();
@@ -189,12 +173,12 @@ var Dimension = Model.extend(/** @lends module:model/dimension.prototype */{
     },
 
     /**
-     * Judge overflow
-     * @param {{x: number, y: number}} containerPosition - Position values based on container
-     * @param {{height: number, totalWidth: number, rsideWidth: number}} bodySize - Real body size
-     * @returns {{x: number, y: number}} Overflow values
-     * @private
-     */
+   * Judge overflow
+   * @param {{x: number, y: number}} containerPosition - Position values based on container
+   * @param {{height: number, totalWidth: number, rsideWidth: number}} bodySize - Real body size
+   * @returns {{x: number, y: number}} Overflow values
+   * @private
+   */
     _judgeOverflow: function(containerPosition, bodySize) {
         var containerX = containerPosition.x;
         var containerY = containerPosition.y;
@@ -220,29 +204,29 @@ var Dimension = Model.extend(/** @lends module:model/dimension.prototype */{
     },
 
     /**
-     * Return height of X-scrollBar.
-     * If no X-scrollBar, return 0
-     * @returns {number} Height of X-scrollBar
-     */
+   * Return height of X-scrollBar.
+   * If no X-scrollBar, return 0
+   * @returns {number} Height of X-scrollBar
+   */
     getScrollXHeight: function() {
         return (this.get('scrollX') ? this.get('scrollBarSize') : 0);
     },
 
     /**
-     * Return width of Y-scrollBar.
-     * If no Y-scrollBar, return 0
-     * @returns {number} Width of Y-scrollBar
-     */
+   * Return width of Y-scrollBar.
+   * If no Y-scrollBar, return 0
+   * @returns {number} Width of Y-scrollBar
+   */
     getScrollYWidth: function() {
         return (this.get('scrollY') ? this.get('scrollBarSize') : 0);
     },
 
     /**
-     * Returns the height of table body.
-     * @param  {number} height - The height of the dimension
-     * @returns {number} The height of the table body
-     * @private
-     */
+   * Returns the height of table body.
+   * @param  {number} height - The height of the dimension
+   * @returns {number} The height of the table body
+   * @private
+   */
     _calcRealBodyHeight: function(height) {
         var extraHeight = this.get('headerHeight') + this.get('summaryHeight') + TABLE_BORDER_WIDTH;
 
@@ -250,19 +234,19 @@ var Dimension = Model.extend(/** @lends module:model/dimension.prototype */{
     },
 
     /**
-     * Returns the minimum height of table body.
-     * @returns {number} The minimum height of table body
-     * @private
-     */
+   * Returns the minimum height of table body.
+   * @returns {number} The minimum height of table body
+   * @private
+   */
     _getMinBodyHeight: function() {
         return this.get('minBodyHeight') + (CELL_BORDER_WIDTH * 2) + this.getScrollXHeight();
     },
 
     /**
-     * 열 고정 영역의 minimum width 값을 구한다.
-     * @returns {number} 열고정 영역의 최소 너비값.
-     * @private
-     */
+   * 열 고정 영역의 minimum width 값을 구한다.
+   * @returns {number} 열고정 영역의 최소 너비값.
+   * @private
+   */
     _getMinLeftSideWidth: function() {
         var minimumColumnWidth = this.get('minimumColumnWidth');
         var columnFrozenCount = this.columnModel.getVisibleFrozenCount(true);
@@ -278,10 +262,10 @@ var Dimension = Model.extend(/** @lends module:model/dimension.prototype */{
     },
 
     /**
-     * 열 고정 영역의 maximum width 값을 구한다.
-     * @returns {number} 열고정 영역의 최대 너비값.
-     * @private
-     */
+   * 열 고정 영역의 maximum width 값을 구한다.
+   * @returns {number} 열고정 영역의 최대 너비값.
+   * @private
+   */
     getMaxLeftSideWidth: function() {
         var maxWidth = Math.ceil(this.get('width') * 0.9); // eslint-disable-line no-magic-number
 
@@ -293,9 +277,9 @@ var Dimension = Model.extend(/** @lends module:model/dimension.prototype */{
     },
 
     /**
-     * Set the width of the dimension.
-     * @param {number} width - Width
-     */
+   * Set the width of the dimension.
+   * @param {number} width - Width
+   */
     setWidth: function(width) {
         if (width > 0) {
             this.set('width', width);
@@ -304,11 +288,11 @@ var Dimension = Model.extend(/** @lends module:model/dimension.prototype */{
     },
 
     /**
-     * Sets the height of the dimension.
-     * (Resets the bodyHeight relative to the dimension height)
-     * @param  {number} height - The height of the dimension
-     * @private
-     */
+   * Sets the height of the dimension.
+   * (Resets the bodyHeight relative to the dimension height)
+   * @param  {number} height - The height of the dimension
+   * @private
+   */
     setHeight: function(height) {
         if (height > 0) {
             this.set('bodyHeight', Math.max(this._calcRealBodyHeight(height), this._getMinBodyHeight()));
@@ -316,16 +300,16 @@ var Dimension = Model.extend(/** @lends module:model/dimension.prototype */{
     },
 
     /**
-     * Returns the height of the dimension.
-     * @returns {Number} Height
-     */
+   * Returns the height of the dimension.
+   * @returns {Number} Height
+   */
     getHeight: function() {
         return this.get('bodyHeight') + this.get('headerHeight');
     },
 
     /**
-     * layout 에 필요한 크기 및 위치 데이터를 갱신한다.
-     */
+   * layout 에 필요한 크기 및 위치 데이터를 갱신한다.
+   */
     refreshLayout: function() {
         var domState = this.domState;
         var offset = domState.getOffset();
@@ -342,24 +326,24 @@ var Dimension = Model.extend(/** @lends module:model/dimension.prototype */{
     },
 
     /**
-     * Returns the offset.top of body
-     * @returns    {number}
-     */
+   * Returns the offset.top of body
+   * @returns    {number}
+   */
     getBodyOffsetTop: function() {
         var offsetTop = this.domState.getOffset().top;
         var summaryHeight = this.get('summaryPosition') === summaryPositionConst.TOP ? this.get('summaryHeight') : 0;
 
         return offsetTop + this.get('headerHeight') + summaryHeight
-            + CELL_BORDER_WIDTH + TABLE_BORDER_WIDTH;
+      + CELL_BORDER_WIDTH + TABLE_BORDER_WIDTH;
     },
 
     /**
-     * Returns the position relative to the body-area.
-     * @param {Number} pageX - x-pos relative to document
-     * @param {Number} pageY - y-pos relative to document
-     * @returns {{x: number, y: number}}
-     * @private
-     */
+   * Returns the position relative to the body-area.
+   * @param {Number} pageX - x-pos relative to document
+   * @param {Number} pageY - y-pos relative to document
+   * @returns {{x: number, y: number}}
+   * @private
+   */
     getPositionFromBodyArea: function(pageX, pageY) {
         var bodyOffsetX = this.domState.getOffset().left;
         var bodyOffsetY = this.getBodyOffsetTop();
