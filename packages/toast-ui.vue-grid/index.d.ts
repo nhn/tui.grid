f1ce1e668ead85e2903fbfcdbe5597c3844e72c5
김동우 <dongwoo.kim@nhnent.com>
2019-03-27 12:49:04 +0900

    feat: add type definition (#4)
    * chore: add prettier config
    
    * type: add type definition
    
    * chore: upgrade version to v1.0.5
    
    * chore: update package-lock.json

---
+++ packages/toast-ui.vue-grid/index.d.ts
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import TuiGrid from 'tui-grid';
+
+type FunctionKeys<T extends object> = {[K in keyof T]: T[K] extends Function ? K : never}[keyof T];
+
+type GridFnKeys = FunctionKeys<TuiGrid>;
+
+export declare class Grid extends Vue {
+  invoke<T extends GridFnKeys>(fname: T, ...args: Parameters<TuiGrid[T]>): ReturnType<TuiGrid[T]>;
+  getRootElement(): HTMLElement;
+}
