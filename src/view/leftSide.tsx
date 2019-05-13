928f57c5133e65672a22e1a8ad3351ea9f3d0fc8
js87zz <js87zz.lee@nhn.com>
2019-05-13 11:18:05 +0900

    feat: add summaryArea component in content

--- src/view/leftSide.tsx
+++ src/view/leftSide.tsx
@@ -1,13 +1,16 @@
 import { h, Component } from 'preact';
 import { HeadArea } from './headArea';
 import { BodyArea } from './bodyArea';
+import { SummaryArea } from './summaryArea';
 import { cls } from '../helper/dom';
 import { connect } from './hoc';
 import { DispatchProps } from '../dispatch/create';
+import { SummaryPosition } from '../store/types';
 
 interface StoreProps {
   width: number;
   scrollX: boolean;
+  summaryPosition: SummaryPosition;
 }
 
 type Props = StoreProps & DispatchProps;
@@ -15,11 +18,14 @@ type Props = StoreProps & DispatchProps;
 class LeftSideComp extends Component<Props> {
   public render({ width, scrollX }: Props) {
     const style = { width, display: 'block' };
+    const { summaryPosition } = this.props;
 
     return (
       <div class={cls('lside-area')} style={style}>
         <HeadArea side="L" />
+        {summaryPosition === 'top' && <SummaryArea side="L" />}
         <BodyArea side="L" />
+        {summaryPosition === 'bottom' && <SummaryArea side="L" />}
         {scrollX && <div class={cls('scrollbar-left-bottom')} />}
       </div>
     );
@@ -28,5 +34,6 @@ class LeftSideComp extends Component<Props> {
 
 export const LeftSide = connect<StoreProps>(({ columnCoords, dimension }) => ({
   width: columnCoords.areaWidth.L,
-  scrollX: dimension.scrollX
+  scrollX: dimension.scrollX,
+  summaryPosition: dimension.summaryPosition
 }))(LeftSideComp);
