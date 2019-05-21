0f1b9d8d5dd10bfc347105f43f11787d7132b8a2
seonim-ryu <seonim.ryu@nhn.com>
2019-05-21 11:59:42 +0900

    chore: change spec from string to template literal

--- stories/validation.stories.ts
+++ stories/validation.stories.ts
@@ -38,11 +38,11 @@ stories.add(
     const rootEl = document.createElement('div');
     rootEl.appendChild(el);
 
-    button("setValue(0, 'name', '')", () => grid.setValue(0, 'name', ''));
-    button("setValue(0, 'artist', '')", () => grid.setValue(0, 'artist', ''));
+    button(`setValue(0, 'name', '')`, () => grid.setValue(0, 'name', ''));
+    button(`setValue(0, 'artist', '')`, () => grid.setValue(0, 'artist', ''));
 
-    button("setValue(2, 'name', '')", () => grid.setValue(2, 'name', ''));
-    button("setValue(2, 'artist', '')", () => grid.setValue(2, 'artist', ''));
+    button(`setValue(2, 'name', '')`, () => grid.setValue(2, 'name', ''));
+    button(`setValue(2, 'artist', '')`, () => grid.setValue(2, 'artist', ''));
 
     button('validate()', () => alertObjectToJson(grid.validate(), true));
 
@@ -66,15 +66,15 @@ stories.add(
     const rootEl = document.createElement('div');
     rootEl.appendChild(el);
 
-    button("setValue(0, 'name', 'foo')", () => grid.setValue(0, 'name', 'foo'));
-    button("setValue(0, 'name', '100')", () => grid.setValue(0, 'name', '100'));
-    button("setValue(0, 'name', 10)", () => grid.setValue(0, 'name', 10));
-    button("setValue(0, 'name', false)", () => grid.setValue(0, 'name', false));
+    button(`setValue(0, 'name', 'foo')`, () => grid.setValue(0, 'name', 'foo'));
+    button(`setValue(0, 'name', '100')`, () => grid.setValue(0, 'name', '100'));
+    button(`setValue(0, 'name', 10)`, () => grid.setValue(0, 'name', 10));
+    button(`setValue(0, 'name', false)`, () => grid.setValue(0, 'name', false));
 
-    button("setValue(2, 'artist', 'foo')", () => grid.setValue(2, 'artist', 'foo'));
-    button("setValue(2, 'artist', '100')", () => grid.setValue(2, 'artist', '100'));
-    button("setValue(2, 'artist', 10)", () => grid.setValue(2, 'artist', 10));
-    button("setValue(2, 'artist', false)", () => grid.setValue(2, 'artist', false));
+    button(`setValue(2, 'artist', 'foo')`, () => grid.setValue(2, 'artist', 'foo'));
+    button(`setValue(2, 'artist', '100')`, () => grid.setValue(2, 'artist', '100'));
+    button(`setValue(2, 'artist', 10)`, () => grid.setValue(2, 'artist', 10));
+    button(`setValue(2, 'artist', false)`, () => grid.setValue(2, 'artist', false));
 
     button('validate()', () => alertObjectToJson(grid.validate(), true));
 
@@ -98,15 +98,15 @@ stories.add(
     const rootEl = document.createElement('div');
     rootEl.appendChild(el);
 
-    button("setValue(0, 'downloadCount', 'foo')", () => grid.setValue(0, 'downloadCount', 'foo'));
-    button("setValue(0, 'downloadCount', '100')", () => grid.setValue(0, 'downloadCount', '100'));
-    button("setValue(0, 'downloadCount', 10)", () => grid.setValue(0, 'downloadCount', 10));
-    button("setValue(0, 'downloadCount', false)", () => grid.setValue(0, 'downloadCount', false));
+    button(`setValue(0, 'downloadCount', 'foo')`, () => grid.setValue(0, 'downloadCount', 'foo'));
+    button(`setValue(0, 'downloadCount', '100')`, () => grid.setValue(0, 'downloadCount', '100'));
+    button(`setValue(0, 'downloadCount', 10)`, () => grid.setValue(0, 'downloadCount', 10));
+    button(`setValue(0, 'downloadCount', false)`, () => grid.setValue(0, 'downloadCount', false));
 
-    button("setValue(2, 'listenCount', 'foo')", () => grid.setValue(2, 'listenCount', 'foo'));
-    button("setValue(2, 'listenCount', '100')", () => grid.setValue(2, 'listenCount', '100'));
-    button("setValue(2, 'listenCount', 10)", () => grid.setValue(2, 'listenCount', 10));
-    button("setValue(2, 'listenCount', false)", () => grid.setValue(2, 'listenCount', false));
+    button(`setValue(2, 'listenCount', 'foo')`, () => grid.setValue(2, 'listenCount', 'foo'));
+    button(`setValue(2, 'listenCount', '100')`, () => grid.setValue(2, 'listenCount', '100'));
+    button(`setValue(2, 'listenCount', 10)`, () => grid.setValue(2, 'listenCount', 10));
+    button(`setValue(2, 'listenCount', false)`, () => grid.setValue(2, 'listenCount', false));
 
     button('validate()', () => alertObjectToJson(grid.validate(), true));
 
