7ea15cb4ca8ed7106d413f6b3a373c5c9ed4f0b6
js87zz <js87zz.lee@nhn.com>
2019-06-10 10:17:31 +0900

    fix: isEmpty method parameter type

--- src/helper/common.ts
+++ src/helper/common.ts
@@ -251,15 +251,15 @@ export function isString(value: unknown): value is string {
 }
 
 /**
- * check the emptiness of object or array. if obj parameter is null or undefind, return true
+ * check the emptiness(included null) of object or array. if obj parameter is null or undefind, return true
  * @param obj - target object or array
  * @returns the emptiness of obj
  */
-export function isEmpty(obj: object | any[]) {
+export function isEmpty(obj: any) {
   if (
     isNull(obj) ||
     isUndefined(obj) ||
-    (!isUndefined((obj as any[]).length) && (obj as any[]).length === 0) ||
+    (!isUndefined(obj.length) && obj.length === 0) ||
     Object.keys(obj).length === 0
   ) {
     return true;
