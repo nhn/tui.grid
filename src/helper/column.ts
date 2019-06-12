5e9925c007baab1cec0595b228767f60d213a0f5
한정 <hanjung@nhn.com>
2019-06-12 16:31:15 +0900

    feat: complex header selection (#416)
    * feat: add complex header selection
    
    * chore: remove unused code
    
    * chore: apply review - 1

--- src/helper/column.ts
+++ src/helper/column.ts
@@ -1,3 +1,6 @@
+import { ComplexColumnInfo } from '../store/types';
+import { some } from './common';
+
 export function isRowHeader(columnName: string) {
   return ['_number', '_checked'].indexOf(columnName) > -1;
 }
@@ -8,4 +11,8 @@ export function isRowNumColumn(columnName: string) {
 
 export function isCheckboxColumn(columnName: string) {
   return columnName === '_checked';
+}
+
+export function isParentColumnHeader(complexHeaderColumns: ComplexColumnInfo[], name: string) {
+  return !!complexHeaderColumns.length && some((item) => item.name === name, complexHeaderColumns);
 }
