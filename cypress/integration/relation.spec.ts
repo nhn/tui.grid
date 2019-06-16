1a3070347c18b328d680ecfb68e22f8cf6a311f9
이재성 <dlwotjdwkd@ijaeseong-ui-MacBook-Pro.local>
2019-06-16 14:12:12 +0900

    feat: add setColumns API test for relation columns

--- cypress/integration/relation.spec.ts
+++ cypress/integration/relation.spec.ts
@@ -1,47 +1,7 @@
-import Grid from '../../src/grid';
 import { cls } from '../../src/helper/dom';
 import { data, columns } from '../../samples/relations';
 
-interface GridGlobal {
-  tui: { Grid: typeof Grid };
-  grid: Grid;
-}
-
-const CONTENT_WIDTH = 700;
-// @TODO: Retrieve scrollbar-width from real browser
-const SCROLLBAR_WIDTH = 17;
-
-function createDefaultOptions() {
-  return { data, columns };
-}
-
-function createGrid(customOptions: Record<string, unknown> = {}) {
-  cy.window().then((win: Window & Partial<GridGlobal>) => {
-    const { document, tui } = win;
-    const defaultOptions = createDefaultOptions();
-    const options = { ...defaultOptions, ...customOptions };
-    const el = document.createElement('div');
-    el.style.width = `${CONTENT_WIDTH + SCROLLBAR_WIDTH}px`;
-    document.body.appendChild(el);
-
-    win.grid = new tui!.Grid({ el, ...options });
-    cy.wait(10);
-  });
-}
-
-before(() => {
-  cy.visit('/dist');
-});
-
-beforeEach(() => {
-  cy.document().then((doc) => {
-    doc.body.innerHTML = '';
-  });
-});
-
-// @TODO add more test and data
-it('should change state by relations', () => {
-  createGrid();
+function assertRelation() {
   cy.get(`.${cls('container')}`).as('container');
 
   cy.get('@container')
@@ -56,4 +16,32 @@ it('should change state by relations', () => {
   cy.get(`.${cls('layer-editing')} select`)
     .select('Balad/Dance/Pop')
     .should('have.value', '01_01');
+}
+
+before(() => {
+  cy.visit('/dist');
+});
+
+beforeEach(() => {
+  cy.document().then((doc) => {
+    doc.body.innerHTML = '';
+  });
+  cy.createGrid({ data, columns });
+});
+
+// @TODO add more test and data
+it('should change state by relations', () => {
+  assertRelation();
+});
+
+it('set relation columns through setColumns()', () => {
+  cy.document().then((doc) => {
+    doc.body.innerHTML = '';
+  });
+  cy.createGrid({
+    data,
+    columns: [{ header: 'name', name: 'name' }]
+  });
+  cy.gridInstance().invoke('setColumns', columns);
+  assertRelation();
 });
