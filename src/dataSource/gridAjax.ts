7a8c24420f701919b8a0f2985a040ca8515ebf43
jaesung-lee <js87zz.lee@nhn.com>
2019-06-24 19:22:56 +0900

    fix: modify typo (#471)

--- src/dataSource/gridAjax.ts
+++ src/dataSource/gridAjax.ts
@@ -117,7 +117,7 @@ export default class GridAjax {
     } else {
       xhr.open(method, url);
       // @TODO neeto to application/json content-type options and custom options(authorization, custom header etc..)
-      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoder');
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     }
     xhr.onreadystatechange = this.handleReadyStateChange;
   }
