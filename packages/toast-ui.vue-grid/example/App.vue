452440c4d1f7c1c400c0a9f9ca923b2a6f376d36
Sohee Lee <sohee.lee7@nhnent.com>
2019-06-27 15:28:32 +0900

    refactor: tabsize 4 to 2

--- packages/toast-ui.vue-grid/example/App.vue
+++ packages/toast-ui.vue-grid/example/App.vue
@@ -1,142 +1,142 @@
 <template>
-<div class="container">
+  <div class="container">
     <h1>🍞🔡 TOAST UI Grid + Vue</h1>
-    <grid :rowData="data"
-          :columnData="columns"
-          :options="options"
-          @check="onCheck"
-          @uncheck="onUnCheck"
+    <grid
+      :rowData="data"
+      :columnData="columns"
+      :options="options"
+      @check="onCheck"
+      @uncheck="onUnCheck"
     ></grid>
-</div>
+  </div>
 </template>
 <script>
 import {Grid} from '../src/index.js';
 
 export default {
-    components: {
-        'grid': Grid
-    },
-    data() {
-        return {
-            columns: [
-                {
-                    title: 'Name',
-                    name: 'name'
-                },
-                {
-                    title: 'Artist',
-                    name: 'artist'
-                },
-                {
-                    title: 'Personal Score',
-                    name: 'score',
-                    onBeforeChange(ev) {
-                        console.log('executes before the value changes : ', ev);
-                    },
-                    onAfterChange(ev) {
-                        console.log('executes after the value has changed : ', ev);
-                    },
-                    copyOptions: {
-                        useListItemText: true
-                    },
-                    editOptions: {
-                        type: 'radio',
-                        listItems: [
-                            {
-                                text: '★☆☆☆☆',
-                                value: '1'
-                            },
-                            {
-                                text: '★★☆☆☆',
-                                value: '2'
-                            },
-                            {
-                                text: '★★★☆☆',
-                                value: '3'
-                            },
-                            {
-                                text: '★★★★☆',
-                                value: '4'
-                            },
-                            {
-                                text: '★★★★★',
-                                value: '5'
-                            }
-                        ],
-                        useViewMode: true
-                    }
-                }
-            ],
-            data: [
-                {
-                    name: 'Kiss and Make Up',
-                    artist: 'Dua Lipa',
-                    score: '5'
-                },
-                {
-                    name: 'Bohemian Rhapsody',
-                    artist: 'Queen',
-                    score: '2'
-                },
-                {
-                    name: 'Done For Me',
-                    artist: 'Charlie Puth',
-                    score: '3'
-                },
-                {
-                    name: 'thank u, next',
-                    artist: 'Ariana Grande',
-                    score: '4'
-                },
-                {
-                    name: 'Handclap',
-                    artist: 'Fitz & The Tantrums',
-                    score: '1'
-                },
-                {
-                    name: 'Shape Of You',
-                    artist: 'Ed Sheeran',
-                    score: '5'
-                },
-                {
-                    name: 'Snowman',
-                    artist: 'Sia',
-                    score: '5'
-                },
-                {
-                    name: 'Don\'t Stop Me Now ',
-                    artist: 'Queen',
-                    score: '3'
-                },
-                {
-                    name: 'Havana',
-                    artist: 'Camila Cabello',
-                    score: '2'
-                },
-                {
-                    name: 'A No No',
-                    artist: 'Mariah Carey',
-                    score: '5'
-                }
-            ],
-            options: {
-                rowHeaders: [
-                    {
-                        type: 'checkbox'
-                    }
-                ]
-            }
-        };
-    },
-    methods: {
-        onCheck(ev) {
-            console.log('check event: ', ev);
+  components: {
+    grid: Grid
+  },
+  data() {
+    return {
+      columns: [
+        {
+          title: 'Name',
+          name: 'name'
         },
-        onUnCheck(ev) {
-            console.log('uncheck event: ', ev);
+        {
+          title: 'Artist',
+          name: 'artist'
+        },
+        {
+          title: 'Personal Score',
+          name: 'score',
+          onBeforeChange(ev) {
+            console.log('executes before the value changes : ', ev);
+          },
+          onAfterChange(ev) {
+            console.log('executes after the value has changed : ', ev);
+          },
+          copyOptions: {
+            useListItemText: true
+          },
+          editOptions: {
+            type: 'radio',
+            listItems: [
+              {
+                text: '★☆☆☆☆',
+                value: '1'
+              },
+              {
+                text: '★★☆☆☆',
+                value: '2'
+              },
+              {
+                text: '★★★☆☆',
+                value: '3'
+              },
+              {
+                text: '★★★★☆',
+                value: '4'
+              },
+              {
+                text: '★★★★★',
+                value: '5'
+              }
+            ],
+            useViewMode: true
+          }
         }
-
+      ],
+      data: [
+        {
+          name: 'Kiss and Make Up',
+          artist: 'Dua Lipa',
+          score: '5'
+        },
+        {
+          name: 'Bohemian Rhapsody',
+          artist: 'Queen',
+          score: '2'
+        },
+        {
+          name: 'Done For Me',
+          artist: 'Charlie Puth',
+          score: '3'
+        },
+        {
+          name: 'thank u, next',
+          artist: 'Ariana Grande',
+          score: '4'
+        },
+        {
+          name: 'Handclap',
+          artist: 'Fitz & The Tantrums',
+          score: '1'
+        },
+        {
+          name: 'Shape Of You',
+          artist: 'Ed Sheeran',
+          score: '5'
+        },
+        {
+          name: 'Snowman',
+          artist: 'Sia',
+          score: '5'
+        },
+        {
+          name: "Don't Stop Me Now ",
+          artist: 'Queen',
+          score: '3'
+        },
+        {
+          name: 'Havana',
+          artist: 'Camila Cabello',
+          score: '2'
+        },
+        {
+          name: 'A No No',
+          artist: 'Mariah Carey',
+          score: '5'
+        }
+      ],
+      options: {
+        rowHeaders: [
+          {
+            type: 'checkbox'
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    onCheck(ev) {
+      console.log('check event: ', ev);
+    },
+    onUnCheck(ev) {
+      console.log('uncheck event: ', ev);
     }
+  }
 };
 </script>
 <style>
