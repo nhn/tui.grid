0abb7e5dcda192fd8e9328d2dd1a906eab453229
js87zz <js87zz.lee@nhn.com>
2019-09-05 17:16:22 +0900

    chore: add sort example

---
+++ examples/data/sort-dummy.js
@@ -0,0 +1,66 @@
+/* eslint-disable */
+const sortData = [
+  {
+    alphabetA: 'B',
+    alphabetB: 'B',
+    alphabetC: 'A',
+    numberA: 2,
+    numberB: 3
+  },
+  {
+    alphabetA: 'A',
+    alphabetB: 'A',
+    alphabetC: 'A',
+    numberA: 1,
+    numberB: 1
+  },
+  {
+    alphabetA: 'C',
+    alphabetB: 'A',
+    alphabetC: 'C',
+    numberA: 1,
+    numberB: 3
+  },
+  {
+    alphabetA: 'A',
+    alphabetB: 'B',
+    alphabetC: 'C',
+    numberA: 1,
+    numberB: 10
+  },
+  {
+    alphabetA: 'A',
+    alphabetB: 'B',
+    alphabetC: 'C',
+    numberA: 10,
+    numberB: 10
+  },
+  {
+    alphabetA: 'D',
+    alphabetB: 'E',
+    alphabetC: 'C',
+    numberA: 1,
+    numberB: 1
+  },
+  {
+    alphabetA: 'B',
+    alphabetB: 'C',
+    alphabetC: 'C',
+    numberA: 20,
+    numberB: 20
+  },
+  {
+    alphabetA: 'A',
+    alphabetB: 'A',
+    alphabetC: 'A',
+    numberA: 24,
+    numberB: 24
+  },
+  {
+    alphabetA: 'F',
+    alphabetB: 'F',
+    alphabetC: 'F',
+    numberA: 25,
+    numberB: 25
+  }
+];
