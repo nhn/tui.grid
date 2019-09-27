7eb9b74e0ff679f5c1d935772cc0b24a04b170f1
js87zz <js87zz.lee@nhn.com>
2019-09-27 18:09:17 +0900

    fix: import cypress tab type

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,11 +1,9 @@
 /// <reference path="../../node_modules/cypress/types/sinon/index.d.ts" />
+/// <reference path="../../node_modules/cypress-plugin-tab/src/index.d.ts" />
 /// <reference path="../../types/tui-pagination/index.d.ts" />
 /// <reference path="../../types/tui-date-picker/index.d.ts" />
 /// <reference types="cypress" />
 
-interface Option {
-  shift: boolean;
-}
 declare namespace Cypress {
   interface Chainable<Subject> {
     getCell(rowKey: number | string, column: string): Chainable<any>;
@@ -21,7 +19,5 @@ declare namespace Cypress {
     gridInstance(): Chainable<any>;
 
     stub(): Agent<sinon.SinonStub> & sinon.SinonStub;
-
-    tab(options?: Option): Chainable<any>;
   }
 }
