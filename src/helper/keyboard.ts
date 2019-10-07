c95e3440fdbc67635639f053bc07d4c4dfacb1cc
js87zz <js87zz.lee@nhn.com>
2019-10-07 11:44:43 +0900

    chore: change variable name

--- src/helper/keyboard.ts
+++ src/helper/keyboard.ts
@@ -188,6 +188,6 @@ export function getNextRowIndex(rowIndex: number, heights: number[]) {
 
 export function isNotFilterKey(keyCode: number) {
   // shift, ctrl, esc, left, up, right, down, pageUp, pageDown, end, home
-  const fnKeys = [16, 17, 27, 37, 38, 39, 40, 33, 34, 35, 36];
-  return includes(fnKeys, keyCode);
+  const keys = [16, 17, 27, 37, 38, 39, 40, 33, 34, 35, 36];
+  return includes(keys, keyCode);
 }
