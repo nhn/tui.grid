d4b338b4145dfe11a127337e57393d658dc79bf7
js87zz <js87zz.lee@nhn.com>
2019-11-19 16:26:01 +0900

    refactor: simplify patchArrayMethods function

--- packages/toast-ui.grid/src/helper/array.ts
+++ packages/toast-ui.grid/src/helper/array.ts
@@ -1,51 +1,38 @@
 import { arrayEqual } from './common';
-import { Dictionary } from '../store/types';
 import { notify } from './observable';
+import { Dictionary } from 'src/store/types';
 
-type PatchingMethodMap = {
-  splice: Function;
-  pop: Function;
-  push: Function;
-  sort: Function;
+type ArrayProtoProps = keyof typeof Array.prototype;
+type Methods = ArrayProtoProps[];
+
+const methods: Methods = ['splice', 'push', 'pop', 'shift', 'unshift', 'sort'];
+const derivedProps: Dictionary<string[]> = {
+  rawData: ['rawData', 'filteredRawData'],
+  viewData: ['viewData', 'filteredViewData']
 };
 
-const methods = ['splice', 'pop', 'push', 'sort'] as const;
-const patchedMethodMap = {} as Dictionary<PatchingMethodMap>;
-
-function fillPachedMethodMap(arr: any[], key: string) {
-  let patchedMethods = patchedMethodMap[key as string];
-
-  methods.forEach(method => {
-    if (!patchedMethodMap[key as string]) {
-      patchedMethods = patchedMethodMap[key as string] = {} as PatchingMethodMap;
-    }
-    if (!patchedMethods[method]) {
-      patchedMethods[method] = arr[method];
-    }
-  });
+function isSortMethod(method: ArrayProtoProps) {
+  return method === 'sort';
 }
 
-export function patchArrayMethods<T>(arr: any[], obj: T, key: keyof T) {
-  fillPachedMethodMap(arr, key as string);
-  const patchedMethods = patchedMethodMap[key as string];
+export function patchArrayMethods<T>(arr: any[], obj: T, key: string) {
+  const arrayProto = Array.prototype;
 
   methods.forEach(method => {
-    if (method === 'sort') {
-      arr[method] = function(...args: any) {
-        const copied = [...this];
-        const result = patchedMethods[method].apply(this, args);
-        if (!arrayEqual(copied, this)) {
-          notify(obj, key);
+    const keys = (derivedProps[key] || [key]) as (keyof T)[];
+    arr[method] = function patch(...args: any[]) {
+      const copied = isSortMethod(method) ? [...this] : null;
+      const result = arrayProto[method].apply(this, args);
+
+      if (isSortMethod(method)) {
+        if (!arrayEqual(copied!, this)) {
+          notify(obj, ...keys);
         }
-        return result;
-      };
-    } else {
-      arr[method] = function(...args: any) {
-        const result = patchedMethods[method].apply(this, args);
-        notify(obj, key);
-        return result;
-      };
-    }
+      } else {
+        notify(obj, ...keys);
+      }
+      return result;
+    };
   });
 
   return arr;
