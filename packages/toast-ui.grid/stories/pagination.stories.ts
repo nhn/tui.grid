77fcbd9efd2d5ee768127036bfd3c30d5d0a4fe4
js87zz <js87zz.lee@nhn.com>
2019-11-22 10:27:00 +0900

    refactor: client pagination story, test

--- packages/toast-ui.grid/stories/pagination.stories.ts
+++ packages/toast-ui.grid/stories/pagination.stories.ts
@@ -1,46 +1,26 @@
-import { storiesOf } from '@storybook/html';
-import { withKnobs } from '@storybook/addon-knobs';
 import Grid from '../src/grid';
-import { OptGrid } from '../src/types';
-import { Omit } from 'utility-types';
 import { data } from '../samples/pagination';
 import '../src/css/grid.css';
 import 'tui-pagination/dist/tui-pagination.css';
 
-const stories = storiesOf('Pagination', module);
-stories.addDecorator(withKnobs);
+export default {
+  title: 'Pagination'
+};
 
 const columns = [
-  { name: 'deliveryType', sortable: true, sortingType: 'desc' },
-  { name: 'productOrderNo', sortable: true, sortingType: 'asc' },
-  { name: 'orderName', editor: 'text' },
-  { name: 'orderId', editor: 'text' },
-  { name: 'addressee' },
-  { name: 'payState' },
-  { name: 'payType' },
-  { name: 'date' },
-  { name: 'productPrice' },
-  { name: 'deliveryState' },
-  { name: 'deliveryCompany' },
-  { name: 'deliveryPeeType' },
-  { name: 'deliveryNo' }
+  { name: 'deliveryType' },
+  { name: 'productOrderNo' },
+  { name: 'orderName' },
+  { name: 'orderId' },
+  { name: 'addressee' }
 ];
-
-function createGrid(options: Omit<OptGrid, 'el'>) {
-  const el = document.createElement('div');
-  el.style.width = '800px';
-
-  const grid = new Grid({ el, ...options });
-
-  return { el, grid };
-}
 
 const appendData = {
   deliveryType: '-',
   productOrderNo: 0,
-  orderName: '한정',
+  orderName: 'Tester',
   orderId: '-',
-  addressee: '한정',
+  addressee: 'Tester',
   payState: '-',
   payType: '-',
   date: '-',
@@ -51,59 +31,114 @@ const appendData = {
   deliveryNo: 0
 };
 
-stories.add(
-  'client pagination',
-  () => {
-    const { el, grid } = createGrid({
-      data: data.slice(0, 100),
-      columns,
-      bodyHeight: 'fitToParent',
-      rowHeaders: ['rowNum'],
-      columnOptions: {
-        frozenCount: 2,
-        minWidth: 150
-      },
-      pageOptions: {
-        useClient: true,
-        perPage: 10
-      }
-    });
-    const rootEl = document.createElement('div');
-    rootEl.appendChild(el);
-    rootEl.style.height = '400px';
+function createGrid() {
+  const el = document.createElement('div');
+  el.style.width = '800px';
 
-    const appendBtn = document.createElement('button');
-    const prependBtn = document.createElement('button');
-    const removeBtn = document.createElement('button');
-    const clearAllBtn = document.createElement('button');
+  const grid = new Grid({
+    el,
+    data: data.slice(0, 100),
+    columns,
+    pageOptions: {
+      useClient: true,
+      perPage: 10
+    }
+  });
 
-    appendBtn.textContent = 'appendRow';
-    prependBtn.textContent = 'prependRow';
-    removeBtn.textContent = 'removeRow';
-    clearAllBtn.textContent = 'clearAllBtn';
+  return { el, grid };
+}
 
-    appendBtn.addEventListener('click', () => {
-      grid.appendRow(appendData, { at: 3 });
-    });
+function clickLastPageBtnAsync(el: HTMLElement) {
+  setTimeout(() => {
+    const lastBtn: HTMLElement = el.querySelector('.tui-page-btn.tui-last');
+    lastBtn.click();
+  });
+}
 
-    prependBtn.addEventListener('click', () => {
-      grid.prependRow(appendData);
-    });
+export const firstPageBasic = () => {
+  const { el } = createGrid();
+  return el;
+};
 
-    removeBtn.addEventListener('click', () => {
-      grid.removeRow(5);
-    });
+export const lastPageBasic = () => {
+  const { el } = createGrid();
+  clickLastPageBtnAsync(el);
+  return el;
+};
 
-    clearAllBtn.addEventListener('click', () => {
-      grid.clear();
-    });
+export const FirstPageInExceededRange = () => {
+  const { el, grid } = createGrid();
+  grid.appendRow(appendData);
+  return el;
+};
 
-    rootEl.appendChild(appendBtn);
-    rootEl.appendChild(prependBtn);
-    rootEl.appendChild(removeBtn);
-    rootEl.appendChild(clearAllBtn);
+export const lastPageInExceededRange = () => {
+  const { el, grid } = createGrid();
+  grid.appendRow(appendData);
+  clickLastPageBtnAsync(el);
+  return el;
+};
 
-    return rootEl;
-  },
-  { html: { preventForcedRender: true } }
-);
+// export const basic = () => {
+//   const { el, grid } = createGrid();
+//   grid.appendRow(appendData);
+
+//   return el;
+// };
+
+// stories.add(
+//   'client pagination',
+//   () => {
+//     const { el, grid } = createGrid({
+//       data: data.slice(0, 100),
+//       columns,
+//       bodyHeight: 'fitToParent',
+//       rowHeaders: ['rowNum'],
+//       columnOptions: {
+//         frozenCount: 2,
+//         minWidth: 150
+//       },
+//       pageOptions: {
+//         useClient: true,
+//         perPage: 10
+//       }
+//     });
+//     const rootEl = document.createElement('div');
+//     rootEl.appendChild(el);
+//     rootEl.style.height = '400px';
+
+//     const appendBtn = document.createElement('button');
+//     const prependBtn = document.createElement('button');
+//     const removeBtn = document.createElement('button');
+//     const clearAllBtn = document.createElement('button');
+
+//     appendBtn.textContent = 'appendRow';
+//     prependBtn.textContent = 'prependRow';
+//     removeBtn.textContent = 'removeRow';
+//     clearAllBtn.textContent = 'clearAllBtn';
+
+//     appendBtn.addEventListener('click', () => {
+//       grid.appendRow(appendData, { at: 3 });
+//     });
+
+//     prependBtn.addEventListener('click', () => {
+//       grid.prependRow(appendData);
+//     });
+
+//     removeBtn.addEventListener('click', () => {
+//       grid.removeRow(5);
+//     });
+
+//     clearAllBtn.addEventListener('click', () => {
+//       grid.clear();
+//     });
+
+//     rootEl.appendChild(appendBtn);
+//     rootEl.appendChild(prependBtn);
+//     rootEl.appendChild(removeBtn);
+//     rootEl.appendChild(clearAllBtn);
+
+//     return rootEl;
+//   },
+//   { html: { preventForcedRender: true } }
+// );
