6805b55915b772742896e791af2ac6a25642dcbf
js87zz <js87zz.lee@nhn.com>
2019-12-12 18:21:12 +0900

    refactor: add storybook decorator

--- packages/toast-ui.grid/.storybook/config.js
+++ packages/toast-ui.grid/.storybook/config.js
@@ -1,3 +1,14 @@
-import { configure } from '@storybook/html';
+import { configure, addDecorator } from '@storybook/html';
+import { makeDecorator } from '@storybook/addons';
+import Grid from '../src/grid';
 
+const withDefaultTheme =  makeDecorator({
+  name: 'withDefaultTheme',
+  wrapper: (getStory, context) => {
+    Grid.applyTheme({ preset: 'default' });
+    return getStory(context);
+  }
+});
+
+addDecorator(withDefaultTheme);
 configure(require.context('../stories', true, /.stories.tsx?$/), module);
