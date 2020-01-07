9e5651dcea2e02550c1addfbd458f9fa68ab5c1e
js87zz <js87zz.lee@nhn.com>
2020-01-07 20:00:20 +0900

    fix: modify serializing way

---
+++ packages/toast-ui.grid/src/dataSource/helper/serializer.ts
@@ -0,0 +1,51 @@
+import { isObject, isUndefined, isNull } from '../../helper/common';
+import { Params } from '../types';
+import { Dictionary } from '../../store/types';
+
+/**
+ * 1. Array format
+ *
+ * The default array format to serialize is 'bracket'.
+ * However in case of nested array, only the deepest format follows the 'bracket', the rest follow 'indice' format.
+ *
+ * - basic
+ *   { a: [1, 2, 3] } => a[]=1&a[]=2&a[]=3
+ * - nested
+ *   { a: [1, 2, [3]] } => a[]=1&a[]=2&a[2][]=3
+ *
+ * 2. Object format
+ * The default object format to serialize is 'bracket' notation and doesn't allow the 'dot' notation.
+ *
+ * - basic
+ *   { a: { b: 1, c: 2 } } => a[b]=1&a[c]=2
+ */
+function encodePairs(key: string, value: string) {
+  return `${encodeURIComponent(key)}=${encodeURIComponent(
+    isNull(value) || isUndefined(value) ? '' : value
+  )}`;
+}
+
+function serializeParams(key: string, value: any, serializedList: string[]) {
+  if (Array.isArray(value)) {
+    value.forEach((arrVal, index) => {
+      serializeParams(`${key}[${isObject(arrVal) ? index : ''}]`, arrVal, serializedList);
+    });
+  } else if (isObject(value)) {
+    Object.keys(value).forEach(objKey => {
+      serializeParams(`${key}[${objKey}]`, (value as Dictionary<any>)[objKey], serializedList);
+    });
+  } else {
+    serializedList.push(encodePairs(key, value));
+  }
+}
+
+export function serialize(params: Params) {
+  if (!params) {
+    return '';
+  }
+  const serializedList: string[] = [];
+  Object.keys(params).forEach(key => {
+    serializeParams(key, params[key], serializedList);
+  });
+  return serializedList.join('&');
+}
