878bd3a4fbf2d2dd348d827639aee1b3804da34d
js87zz <js87zz.lee@nhn.com>
2020-01-07 20:00:33 +0900

    chore: add serialize test case

---
+++ packages/toast-ui.grid/cypress/integration/serialize.spec.ts
@@ -0,0 +1,20 @@
+import { serialize } from '@/dataSource/helper/serializer';
+
+it('test', () => {
+  const obj = {
+    a: 1,
+    b: [1, 2, 3],
+    c: {
+      d: 'data'
+    },
+    e: [
+      {
+        f: 'nested data'
+      }
+    ],
+    k: null,
+    p: [1, [['2 ']]]
+  };
+  const serialized = serialize(obj);
+  expect(serialized).to.eq(Cypress.$.param(obj));
+});
