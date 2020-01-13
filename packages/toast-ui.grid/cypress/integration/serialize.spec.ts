f63cc8f81dfe30c15f7f79c8ff863c8ebc5e5150
js87zz <js87zz.lee@nhn.com>
2020-01-13 17:57:06 +0900

    chore: add serializer test

--- packages/toast-ui.grid/cypress/integration/serialize.spec.ts
+++ packages/toast-ui.grid/cypress/integration/serialize.spec.ts
@@ -1,20 +1,47 @@
 import { serialize } from '@/dataSource/ajax/serializer';
 
-it('test', () => {
-  const obj = {
-    a: 1,
-    b: [1, 2, 3],
-    c: {
-      d: 'data'
-    },
-    e: [
-      {
-        f: 'nested data'
+function encode(target: string) {
+  return target.replace(/\[|\]/g, (param: string) => encodeURIComponent(param));
+}
+
+describe('serialize array format', () => {
+  it('basic array format is bracket format', () => {
+    const obj = { a: [1, 2, 3] };
+    const serialized = serialize(obj);
+    const encoded = encode('a[]=1&a[]=2&a[]=3');
+
+    expect(serialized).eq(encoded);
+  });
+
+  it('nested array format is mixed by indice and bracket format', () => {
+    const obj = { a: [[1], 2] };
+    const serialized = serialize(obj);
+    const encoded = encode('a[0][]=1&a[]=2');
+
+    expect(serialized).eq(encoded);
+  });
+});
+
+describe('serialize object format', () => {
+  it('basic object format is bracket format', () => {
+    const obj = {
+      a: {
+        b: 1,
+        c: { nested: 2 }
       }
-    ],
-    k: null,
-    p: [1, [['2 ']]]
-  };
+    };
+    const serialized = serialize(obj);
+    const encoded = encode('a[b]=1&a[c][nested]=2');
+
+    expect(serialized).eq(encoded);
+  });
+});
+
+it('null or undfined value is replaced to empty string', () => {
+  // eslint-disable-next-line
+  const obj = { a: null, b: undefined };
   const serialized = serialize(obj);
-  expect(serialized).to.eq(Cypress.$.param(obj));
+  const encoded = encode('a=&b=');
+
+  expect(serialized).eq(encoded);
 });
