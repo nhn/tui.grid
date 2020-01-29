183d490a9d11aa40c40c4c600dd3e903219ba388
js87zz <js87zz.lee@nhn.com>
2020-01-29 19:57:26 +0900

    chore: remove eslint-loader in storybook webpack

--- packages/toast-ui.grid/.storybook/webpack.config.js
+++ packages/toast-ui.grid/.storybook/webpack.config.js
@@ -7,12 +7,6 @@ module.exports = ({ config, mode }) => {
       exclude: /node_modules/,
       use: [
         {
-          enforce: 'pre',
-          test: /\.tsx?$/,
-          exclude: /node_modules/,
-          loader: 'eslint-loader',
-        },
-        {
           loader: require.resolve('ts-loader'),
           options: {
             transpileOnly: true
