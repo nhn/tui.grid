d6c9f512d1ccba80a5fb42c07a07206a17595fa3
js87zz <js87zz.lee@nhn.com>
2020-02-16 02:32:25 +0900

    fix: calculate editor layer position async

--- packages/toast-ui.grid/src/editor/helper.ts
+++ packages/toast-ui.grid/src/editor/helper.ts
@@ -9,29 +9,33 @@ export function setWrapperPosition(
   childEl?: HTMLElement,
   startBottom = false
 ) {
-  const { innerHeight, innerWidth } = window;
-  const { left, top, bottom } = el.getBoundingClientRect();
-  const wrapperTop = startBottom ? bottom : top + EDITOR_TOP_DIFF;
-  const { height: wrapperHeight, width: wrapperWidth } = wrapper.getBoundingClientRect();
-  let childElHeight = 0;
-  let childElWidth = 0;
+  // Use setTimeout to wait until the DOM element is actually mounted or updated.
+  // For example, when start editing the cell with scroll,
+  // grid needs to wait for detecting the accurate scrolling position.
+  setTimeout(() => {
+    const { innerHeight, innerWidth } = window;
+    const { left, top, bottom } = el.getBoundingClientRect();
+    const wrapperTop = startBottom ? bottom : top + EDITOR_TOP_DIFF;
+    const { height: wrapperHeight, width: wrapperWidth } = wrapper.getBoundingClientRect();
+    let childElHeight = 0;
+    let childElWidth = 0;
 
-  if (childEl) {
-    const { height, width } = childEl.getBoundingClientRect();
-    childElHeight = height;
-    childElWidth = width;
-  }
+    if (childEl) {
+      const { height, width } = childEl.getBoundingClientRect();
+      childElHeight = height;
+      childElWidth = width;
+    }
+    wrapper.style.top = `${
+      wrapperTop + wrapperHeight + childElHeight > innerHeight - SCROLL_BAR_X_HEIGHT
+        ? innerHeight - wrapperHeight - childElHeight - INDENT - SCROLL_BAR_X_HEIGHT
+        : wrapperTop
+    }px`;
 
-  wrapper.style.top = `${
-    wrapperTop + wrapperHeight + childElHeight > innerHeight - SCROLL_BAR_X_HEIGHT
-      ? innerHeight - wrapperHeight - childElHeight - INDENT - SCROLL_BAR_X_HEIGHT
-      : wrapperTop
-  }px`;
-
-  const layerWidth = wrapperWidth || childElWidth;
-  wrapper.style.left = `${
-    left + layerWidth > innerWidth - SCROLL_BAR_Y_WIDTH
-      ? innerWidth - layerWidth - INDENT - SCROLL_BAR_Y_WIDTH
-      : left
-  }px`;
+    const layerWidth = wrapperWidth || childElWidth;
+    wrapper.style.left = `${
+      left + layerWidth > innerWidth - SCROLL_BAR_Y_WIDTH
+        ? innerWidth - layerWidth - INDENT - SCROLL_BAR_Y_WIDTH
+        : left
+    }px`;
+  });
 }
