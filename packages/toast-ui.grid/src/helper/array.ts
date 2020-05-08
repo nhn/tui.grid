37bfa04bdd18bfeb01399ed82393363f33d622c4
js87zz <js87zz.lee@nhn.com>
2020-05-09 03:24:53 +0900

    fix: prevent to stack the patched method recursively

--- packages/toast-ui.grid/src/helper/array.ts
+++ packages/toast-ui.grid/src/helper/array.ts
@@ -8,13 +8,20 @@ const methods: Methods = ['splice', 'push', 'pop', 'shift', 'unshift', 'sort'];
 
 export function patchArrayMethods<T>(arr: any[], obj: T, key: string) {
   methods.forEach(method => {
-    const patchedMethods = hasOwnProp(arr, method) ? arr[method] : Array.prototype[method];
+    const patchedMethod = hasOwnProp(arr, method) ? arr[method] : Array.prototype[method];
+    const hasDerivedPatchedMethod = !patchedMethod.registered && hasOwnProp(arr, method);
 
-    arr[method] = function patch(...args: any[]) {
-      const result = patchedMethods.apply(this, args);
-      notify(obj, key as keyof T);
-      return result;
-    };
+    // To prevent to stack the patched method recursively
+    if (hasDerivedPatchedMethod || !hasOwnProp(arr, method)) {
+      arr[method] = function patch(...args: any[]) {
+        const result = patchedMethod.apply(this, args);
+        notify(obj, key as keyof T);
+        return result;
+      };
+      if (hasDerivedPatchedMethod) {
+        arr[method].registered = true;
+      }
+    }
   });
 
   return arr;
