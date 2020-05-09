9c517b1967619911870a9c7c9754dc3e92de7b6c
js87zz <js87zz.lee@nhn.com>
2020-05-09 14:57:32 +0900

    refactor: hasDerivedPatchedMethod => derivedPatchedMethod

--- packages/toast-ui.grid/src/helper/array.ts
+++ packages/toast-ui.grid/src/helper/array.ts
@@ -9,16 +9,16 @@ const methods: Methods = ['splice', 'push', 'pop', 'shift', 'unshift', 'sort'];
 export function patchArrayMethods<T>(arr: any[], obj: T, key: string) {
   methods.forEach(method => {
     const patchedMethod = hasOwnProp(arr, method) ? arr[method] : Array.prototype[method];
-    const hasDerivedPatchedMethod = !patchedMethod.registered && hasOwnProp(arr, method);
+    const derivedPatchedMethod = !patchedMethod.registered && hasOwnProp(arr, method);
 
     // To prevent to stack the patched method recursively
-    if (hasDerivedPatchedMethod || !hasOwnProp(arr, method)) {
+    if (derivedPatchedMethod || !hasOwnProp(arr, method)) {
       arr[method] = function patch(...args: any[]) {
         const result = patchedMethod.apply(this, args);
         notify(obj, key as keyof T);
         return result;
       };
-      if (hasDerivedPatchedMethod) {
+      if (derivedPatchedMethod) {
         arr[method].registered = true;
       }
     }
