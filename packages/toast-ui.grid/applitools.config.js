d63658a0f90c6c88615fb6c0f1ae663e737f054a
DongWoo Kim <dongwoo.kim@nhnent.com>
2020-05-11 21:02:41 +0900

    test: visual test using applitools

---
+++ packages/toast-ui.grid/applitools.config.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports = {
+  // eslint-disable-next-line no-process-env
+  apiKey: process.env.APPLITOOLS_API_KEY,
+  browser: [
+    { width: 1024, height: 768, name: 'chrome' },
+    { width: 1024, height: 768, name: 'ie11' }
+  ],
+  waitBeforeScreenshot: 500
+};
