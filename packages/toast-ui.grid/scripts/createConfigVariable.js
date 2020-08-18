7f99e3d73e4a2fca8f0fe4c8f6bf4ee2748deb00
js87zz <js87zz.lee@nhn.com>
2020-08-18 17:23:54 +0900

    fix: example-test action script

--- packages/toast-ui.grid/scripts/createConfigVariable.js
+++ packages/toast-ui.grid/scripts/createConfigVariable.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const fs = require('fs');
 const path = require('path');
 const config = require(path.resolve(__dirname, '../tuidoc.config.json'));
@@ -20,6 +21,7 @@ function getTestUrls() {
     }
     return urls;
   }, []);
+
 
   fs.writeFileSync('url.txt', testUrls.join(', '));
 }
