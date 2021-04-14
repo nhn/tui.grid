79724e01fc95f9710075c078b6682e12407bca0e
js87zz <js87zz.lee@nhn.com>
2021-04-14 20:14:48 +0900

    chore: add dummy story(show the cell with scroll)

--- packages/toast-ui.grid/stories/dummyRows.stories.ts
+++ packages/toast-ui.grid/stories/dummyRows.stories.ts
@@ -1,6 +1,7 @@
 import { OptGrid } from '../types/options';
 import Grid from '../src/grid';
 import '../src/css/grid.css';
+import { cls } from '../src/helper/dom';
 
 export default {
   title: 'Dummy Rows',
@@ -24,6 +25,10 @@ function createGrid(options: Omit<OptGrid, 'el'>) {
   return { el, grid };
 }
 
+function getRsideBody(el: HTMLElement) {
+  return el.querySelector(`.${cls('rside-area')} .${cls('body-area')}`)!;
+}
+
 export const dummyRows = () => {
   const { el } = createGrid({
     data,
@@ -42,3 +47,38 @@ const dummyRowsNote = `
 - Show the dummy rows to fill remaining area in grid
 `;
 dummyRows.story = { parameters: { notes: dummyRowsNote } };
+
+const largeColumns = [
+  { name: 'name', minWidth: 200, filter: 'text' as const },
+  { name: 'artist', minWidth: 200 },
+  { name: 'type', minWidth: 200 },
+  { name: 'price', minWidth: 200 },
+  { name: 'release', minWidth: 200 },
+  { name: 'genre', minWidth: 200 },
+];
+
+export const dummyRowsWithScroll = () => {
+  const { el, grid } = createGrid({
+    data,
+    columns: largeColumns,
+    bodyHeight: 400,
+    scrollX: true,
+    showDummyRows: true,
+  });
+  grid.filter('name', [{ code: 'eq', value: 'text' }]);
+  const rootEl = document.createElement('div');
+  rootEl.appendChild(el);
+
+  setTimeout(() => {
+    const rsideBody = getRsideBody(el);
+    rsideBody.scrollLeft = 420;
+  });
+
+  return rootEl;
+};
+
+const dummyRowsWithScrollNote = `
+## Dummy Rows
+- Show the dummy cells when scrollX is located at the farthest right.
+`;
+dummyRowsWithScroll.story = { parameters: { notes: dummyRowsWithScrollNote } };
