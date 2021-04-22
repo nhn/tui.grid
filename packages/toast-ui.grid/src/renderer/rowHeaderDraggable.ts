9abbe15de48b9b27d56d6e79e279bd0f4372b4f3
js87zz <js87zz.lee@nhn.com>
2021-04-22 17:10:48 +0900

    chore: apply code review

--- packages/toast-ui.grid/src/renderer/rowHeaderDraggable.ts
+++ packages/toast-ui.grid/src/renderer/rowHeaderDraggable.ts
@@ -1,6 +1,9 @@
 import { CellRenderer } from '@t/renderer';
 import { cls } from '../helper/dom';
 
+const ROW_COUNT = 3;
+const COL_COUNT = 3;
+
 export class RowHeaderDraggableRenderer implements CellRenderer {
   private el: HTMLDivElement;
 
@@ -10,20 +13,20 @@ export class RowHeaderDraggableRenderer implements CellRenderer {
     el.className = cls('row-header-draggable');
 
     this.el = el;
-    this.createSquares();
+    this.renderDraggableIcon();
   }
 
   getElement() {
     return this.el;
   }
 
-  private createSquares() {
-    for (let i = 0; i < 3; i += 1) {
+  private renderDraggableIcon() {
+    for (let i = 0; i < ROW_COUNT; i += 1) {
       const wrapper = document.createElement('div');
 
       wrapper.style.lineHeight = '0';
 
-      for (let j = 0; j < 3; j += 1) {
+      for (let j = 0; j < COL_COUNT; j += 1) {
         const square = document.createElement('span');
 
         wrapper.appendChild(square);
