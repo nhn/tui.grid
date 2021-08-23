1038e9de2383c755d856949201308159c61013b9
js87zz <js87zz.lee@nhn.com>
2021-08-23 13:37:07 +0900

    chore: apply code review

--- packages/toast-ui.grid/cypress/integration/keyMap.spec.ts
+++ packages/toast-ui.grid/cypress/integration/keyMap.spec.ts
@@ -9,7 +9,11 @@ before(() => {
   cy.visit('/dist');
 });
 
-beforeEach(() => {
+function assertEditFinished() {
+  cy.getByCls('content-text').should('not.exist');
+}
+
+function createGrid() {
   const data = [
     { name: 'Han', value: 1 },
     { name: 'Kim', value: 2 },
@@ -23,13 +27,13 @@ beforeEach(() => {
   ];
 
   cy.createGrid({ data, columns });
-});
-
-function assertEditFinished() {
-  cy.getByCls('content-text').should('not.exist');
 }
 
 describe('editor', () => {
+  beforeEach(() => {
+    createGrid();
+  });
+
   it('start and finish editing by pressing enter', () => {
     cy.getCellByIdx(1, 0).click();
     clipboardType('{enter}');
@@ -62,6 +66,10 @@ describe('editor', () => {
 });
 
 describe('Focus', () => {
+  beforeEach(() => {
+    createGrid();
+  });
+
   it('Move by pressing arrow key', () => {
     cy.getCellByIdx(1, 0).click();
     clipboardType('{rightarrow}');
@@ -126,6 +134,8 @@ describe('Focus', () => {
 
 describe('Selection', () => {
   it('select by pressing shift + arrowKey', () => {
+    createGrid();
+
     cy.getCellByIdx(1, 0).click();
     clipboardType('{shift}{rightarrow}');
 
@@ -145,6 +155,8 @@ describe('Selection', () => {
   });
 
   it('Select to the first cell of row based on focus by pressing shift + home', () => {
+    createGrid();
+
     cy.getCellByIdx(0, 1).click();
     clipboardType('{shift}{home}');
 
@@ -152,6 +164,8 @@ describe('Selection', () => {
   });
 
   it('Select to the last cell of row based on focus  by pressing shift + end', () => {
+    createGrid();
+
     cy.getCellByIdx(0, 0).click();
     clipboardType('{shift}{end}');
 
@@ -159,6 +173,8 @@ describe('Selection', () => {
   });
 
   it('Select to the first cell of column based on focus by pressing shift + pageup', () => {
+    createGrid();
+
     cy.getCellByIdx(1, 1).click();
     clipboardType('{shift}{pageup}');
 
@@ -166,6 +182,8 @@ describe('Selection', () => {
   });
 
   it('Select to the last cell of column based on focus by pressing shift + pagedown', () => {
+    createGrid();
+
     cy.getCellByIdx(1, 1).click();
     clipboardType('{shift}{pagedown}');
 
@@ -173,6 +191,8 @@ describe('Selection', () => {
   });
 
   it('Select to the first cell of first column based on focus by pressing cmd(ctrl) + shift + home', () => {
+    createGrid();
+
     cy.getCellByIdx(1, 1).click();
     clipboardType('{ctrl}{shift}{home}');
 
@@ -180,6 +200,8 @@ describe('Selection', () => {
   });
 
   it('Select to the last cell of last column based on focus by pressing cmd(ctrl) + shift + end', () => {
+    createGrid();
+
     cy.getCellByIdx(1, 1).click();
     clipboardType('{ctrl}{shift}{end}');
 
@@ -188,6 +210,8 @@ describe('Selection', () => {
 
   describe('cmd(ctrl) + A', () => {
     it('should Select all cells', () => {
+      createGrid();
+
       cy.getCellByIdx(1, 1).click();
       clipboardType('{ctrl}A');
 
@@ -206,7 +230,6 @@ describe('Selection', () => {
         { name: 'value', editor: 'text' },
       ];
 
-      cy.gridInstance().invoke('destroy');
       cy.createGrid({ columns });
 
       cy.gridInstance().invoke('appendRows', data);
@@ -219,6 +242,8 @@ describe('Selection', () => {
 
   ['backspace', 'del'].forEach((key) => {
     it(`delete selection content by pressing ${key}`, () => {
+      createGrid();
+
       const range = { start: [0, 0], end: [1, 1] };
       cy.gridInstance().invoke('setSelectionRange', range);
       clipboardType(`{${key}}`);
