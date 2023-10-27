f48c91417a1f1f1c7dd7ccb5e02895f15af1cfe4
daeyeon.kim <daeyeon.kim@nhn.com>
2023-10-27 11:09:53 +0900

    chore: update version to v4.21.18

--- packages/toast-ui.grid/types/index.d.ts
+++ packages/toast-ui.grid/types/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for TOAST UI Grid v4.21.17
+// Type definitions for TOAST UI Grid v4.21.18
 // TypeScript Version: 3.9.5
 
 import { CellValue, InvalidRow, Row, RowKey, RowSpan, SortState } from './store/data';
