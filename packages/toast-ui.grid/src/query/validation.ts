2cd5e7070e5a4d4889fe883c975618c62036c4bd
김대연 <daeyeon.kim@nhn.com>
2023-11-16 13:06:19 +0900

    feat: validate on specific rows (#1984)
    * test: add test
    
    * feat: validate on specific rows
    
    * chore: apply code review

--- packages/toast-ui.grid/src/query/validation.ts
+++ packages/toast-ui.grid/src/query/validation.ts
@@ -1,23 +1,27 @@
 import { Store } from '@t/store';
-import { InvalidRow } from '@t/store/data';
+import { InvalidRow, RowKey } from '@t/store/data';
 import { makeObservable } from '../dispatch/data';
 import { isObservable } from '../helper/observable';
-import { createObservableData } from '../dispatch/lazyObservable';
 
-export function getInvalidRows(store: Store) {
-  // @TODO: find more practical way to make observable
-  createObservableData(store, true);
-
+export function getInvalidRows(store: Store, rowKeys?: RowKey[]) {
   const { data, column } = store;
   const invalidRows: InvalidRow[] = [];
 
   data.rawData.forEach((row, rowIndex) => {
-    if (!isObservable(row)) {
+    const needToValidateRow = !rowKeys || rowKeys.includes(row.rowKey);
+
+    if (!isObservable(row) && needToValidateRow) {
       makeObservable(store, rowIndex, true);
     }
   });
 
   data.viewData.forEach(({ rowKey, valueMap }) => {
+    const needToValidateRow = !rowKeys || rowKeys.includes(rowKey);
+
+    if (!needToValidateRow) {
+      return;
+    }
+
     const invalidColumns = column.validationColumns.filter(
       ({ name }) => !!valueMap[name].invalidStates.length
     );
